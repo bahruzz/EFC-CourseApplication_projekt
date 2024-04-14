af6db11c8611126606d17d1b8ec7a61afc19d60c
unknown <bahruzea@code.edu.az>
2024-04-14 21:11:28 +0400

    Education Create Controller Fixed

---
+++ EFCourseDbProjekt/EFCourseDbProjekt/Controllers/EducationController.cs
@@ -0,0 +1,85 @@
+﻿using Domain.Models;
+using Service.Helpers.Extensions;
+using Service.Services;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EFCourseDbProjekt.Controllers
+{
+    public class EducationController
+    {
+        private readonly IEducationService _educationService;
+        private readonly GroupService _groupService;
+        public EducationController()
+        {
+            _educationService = new EducationService();
+            _groupService = new GroupService();
+        }
+
+        public async Task CreateAsync()
+        {
+        Education: ConsoleColor.Cyan.WriteConsole("Add Education Name");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ConsoleColor.Red.WriteConsole("Input can't be empty");
+
+                goto Education;
+
+            }
+            if (!Regex.IsMatch(name, @"^[\p{L}\p{M}' \.\-]+$"))
+            {
+                ConsoleColor.Red.WriteConsole("Format is wrong");
+                goto Education;
+            }
+            var result=await _educationService.GetAllAsync();
+            if(result.Any(m=>m.Name.ToLower() == name.ToLower()))
+            {
+                ConsoleColor.Red.WriteConsole("Education is already exist");
+
+                goto Education;
+            }
+           Color: ConsoleColor.Cyan.WriteConsole("Add Color");
+            string color = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(color))
+            {
+                ConsoleColor.Red.WriteConsole("Input can't be empty");
+
+                goto Color;
+
+            }
+            if (!Regex.IsMatch(color, @"^[\p{L}\p{M}' \.\-]+$"))
+            {
+                ConsoleColor.Red.WriteConsole("Color format is wrong");
+                goto Color;
+            }
+            var data = await _educationService.GetAllAsync();
+            if (data.Any(m => m.Color.ToLower() == color.ToLower()))
+            {
+                ConsoleColor.Red.WriteConsole("This color already exist");
+
+                goto Color;
+            }
+            else
+            {
+                try
+                {
+                    DateTime time = DateTime.Now;
+                    await _educationService.CreateAsync(new Education { Name = name.Trim().ToLower(), Color = color.Trim().ToLower(), CreatedDate = time });
+                    ConsoleColor.Green.WriteConsole("Data succesfuly added");
+                }
+                catch (Exception ex)
+                {
+
+                    ConsoleColor.Red.WriteConsole(ex.Message);
+                    goto Color;
+                }
+            }
+        }
+    }
+}
--- EFCourseDbProjekt/EFCourseDbProjekt/Program.cs
+++ EFCourseDbProjekt/EFCourseDbProjekt/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using EFCourseDbProjekt.Controllers;
+using Service.Helpers.Extensions;
+using Service.Helpers.Enums;
+
+EducationController educationController = new EducationController();
+
+while (true)
+{
+    GetMenues();
+Operation: string operationStr = Console.ReadLine();
+    int operation;
+    bool isCorrectOperationFormat = int.TryParse(operationStr, out operation);
+    if (isCorrectOperationFormat)
+    {
+        switch (operation)
+        {
+            case (int)OperationType.EducationCreate:
+                await educationController.CreateAsync();
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+static void GetMenues()
+{
+    ConsoleColor.Cyan.WriteConsole("Choose one operation :  1-Education create, 2- GroupGetAll, 3-Group delete, 4-GetById, 5-GetAllByRoom,6-GetAllByTeacher, 7-SearchGroupByName,8-Student create,9-StudentGetAll,10-Student delete,11-GetStudentById,12-SearchStudentByNameOrSurname,13-GetAllStudentByAge,14-GetAllStudentByGroupId");
+}
